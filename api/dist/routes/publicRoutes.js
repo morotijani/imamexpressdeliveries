8578ce949c770ea756092a51eafddaf485c59da9
Tijani Moro <tjhackx111@gmail.com>
2026-06-04 02:08:21 +0000

    chore: untrack api/dist from git

--- api/dist/routes/publicRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const publicController_1 = require("../controllers/publicController");
-const router = (0, express_1.Router)();
-router.post('/newsletter', publicController_1.subscribeNewsletter);
-router.post('/contact', publicController_1.submitContactForm);
-exports.default = router;
